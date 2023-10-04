d13ce70dc29471d036405aa07fb955e3f69940f5
Pedro Medina <pedrohugomedina@gmail.com>
2023-10-04 20:44:01 -0300

    Recaptcha login

--- src/js/login.js
+++ src/js/login.js
@@ -1,4 +1,14 @@
 let btnLogin = document.querySelector("#btn-loginForm");
+let chk_captcha = document.querySelector("#chk-captcha");
+let animacion = document.querySelector(".animacion");
+
+chk_captcha.addEventListener("click", function () {
+	if (chk_captcha.checked) {
+		btnLogin.removeAttribute("disabled");
+	} else {
+		btnLogin.setAttribute("disabled", "");
+	}
+});
 
 btnLogin.addEventListener("click", function () {
     localStorage.setItem('login', 'true');
